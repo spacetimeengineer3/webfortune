3f72795bf292314bd0a2e298c7e2c4d434056ed0
spacetimeengineer3 <erwibben@gmail.com>
2021-04-30 00:54:18 +0000

    Added flask support. added some basic functions. This is all untested

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import subprocess
 
-result = subprocess.run(['fortune'], stdout=subprocess.PIPE)
+from flask import Flask, render_template, request, session, redirect, url_for
 
-print(result.stdout)
+fortune = subprocess.run(['fortune'], stdout=subprocess.PIPE)
+
+@app.route('/fortune/')
+def index():
+    return fortune
+
+
+#@app.route('/cowsay/')
+#def return stuff
+
+@app.route ('/cowfortune/')
+#run fortune, pipe it into cowsay and return it
+
+@app.route('/')
+def index():
+    return redirect(url_for('fortune'))
