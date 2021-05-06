be34136af82b657543b0909d9d2c7aa8c0db3ade
spacetimeengineer3 <erwibben@gmail.com>
2021-05-06 12:49:33 +0000

    hotfix2

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 git clone https://github.com/spacetimeengineer3/webfortune
 cd webfortune
-docker build -t wibbene/webfortune .
+exec bash
+
+docker build -t wibbene/webfortune webfortune/Dockerfile
 
 docker run -d -p  5000:5000 wibbene/webfortune
 
