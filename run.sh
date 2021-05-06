f0e1681624985fe7938a62c93ce03773c6aa15ee
spacetimeengineer3 <erwibben@gmail.com>
2021-05-06 03:26:19 +0000

    made docker run in detached mode

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ git clone https://github.com/spacetimeengineer3/webfortune
 
 docker build -t wibbene/webfortune
 
-docker run -p 5000:5000 wibbene/webfortune
+docker run -p -d 5000:5000 wibbene/webfortune
 
 
