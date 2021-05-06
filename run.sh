06a4876e89ccc34ab9b29575eb5d9fca09ffafcb
spacetimeengineer3 <erwibben@gmail.com>
2021-05-06 12:42:24 +0000

    fixed script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git clone https://github.com/spacetimeengineer3/webfortune
-
-docker build -t wibbene/webfortune
+cd webfortune
+docker build -t .
 
 docker run -d -p  5000:5000 wibbene/webfortune
 
